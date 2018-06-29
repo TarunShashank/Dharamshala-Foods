21bb4fc387a5bb0e4c0a2eb12ed454e5a39a5652
Tarun Shashank <stn.shashank@gmail.com>
2018-06-29 23:48:46 +0530

    added graddle

--- Screens/Login.js
+++ Screens/Login.js
@@ -6,115 +6,308 @@ import {
     ImageBackground,
     TextInput,
     TouchableOpacity,
-    Image
+    Image,
+    Animated,
+    Dimensions,
+    Keyboard,
+    Platform
 } from 'react-native';
-import { Fonts } from '../src/utils/Fonts';
+
+import { Icon } from 'native-base'
+const SCREEN_HEIGHT = Dimensions.get('window').height
+import * as Animatable from 'react-native-animatable'
 
 export class Login extends Component {
 
     static navigationOptions = {
         header: null
    }
+   
+    constructor() {
+        super()
+
+        this.state = {
+            placeholderText: 'Enter your mobile number'
+        }
+    } 
+
+   componentWillMount(){
+
+        this.loginHeight = new Animated.Value(150)
+
+        this.keyboardWillShowListener = Keyboard.addListener('keyboardWillShow', this.keyboardWillShow)
+
+        this.keyboardWillHideListener = Keyboard.addListener('keyboardWillHide', this.keyboardWillHide)
+
+        this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', this.keyboardWillShow)
+
+        this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this.keyboardWillHide)
+
+        this.keyboardHeight = new Animated.Value(0)
+        this.forwardArrowOpacity = new Animated.Value(0)
+        this.borderBottomWidth = new Animated.Value(0)
+   }
+
+   keyboardWillShow = (event) => {
+
+    if (Platform.OS == 'android') {
+        duration = 100
+    }
+    else {
+        duration = event.duration
+    }
+
+    Animated.parallel([
+
+        Animated.timing(this.keyboardHeight, {
+            duration: duration + 100,
+            toValue: event.endCoordinates.height + 10
+        }),
+        Animated.timing(this.forwardArrowOpacity, {
+            duration: duration,
+            toValue: 1
+        }),
+        Animated.timing(this.borderBottomWidth, {
+            duration: duration,
+            toValue: 1
+        })
+
+    ]).start()
+
+}
+
+keyboardWillHide = (event) => {
+
+    if (Platform.OS == 'android') {
+        duration = 100
+    }
+    else {
+        duration = event.duration
+    }
+
+    Animated.parallel([
+
+        Animated.timing(this.keyboardHeight, {
+            duration: duration + 100,
+            toValue: 0
+        }),
+        Animated.timing(this.forwardArrowOpacity, {
+            duration: duration,
+            toValue: 0
+        }),
+        Animated.timing(this.borderBottomWidth, {
+            duration: event.duration,
+            toValue: 0
+        })
+
+    ]).start()
+}
+
+   increaseHeightOfLogin = () => {
+
+        this.setState({ placeholderText: '092123456789' })
+        Animated.timing(this.loginHeight, {
+            toValue: SCREEN_HEIGHT,
+            duration: 500
+        }).start(() => {
+
+            this.refs.textInputMobile.focus()
+        })
+    }
+
+    decreaseHeightOfLogin = () => {
+
+        Keyboard.dismiss()
+        Animated.timing(this.loginHeight, {
+            toValue: 150,
+            duration: 500
+        }).start()
+    }
 
   render() {
+
+    const headerTextOpacity = this.loginHeight.interpolate({
+        inputRange: [150, SCREEN_HEIGHT],
+        outputRange: [1, 0]
+    })
+    const marginTop = this.loginHeight.interpolate({
+        inputRange: [150, SCREEN_HEIGHT],
+        outputRange: [25, 100]
+    })
+    const headerBackArrowOpacity = this.loginHeight.interpolate({
+        inputRange: [150, SCREEN_HEIGHT],
+        outputRange: [0, 1]
+    })
+    const titleTextLeft = this.loginHeight.interpolate({
+        inputRange: [150, SCREEN_HEIGHT],
+        outputRange: [100, 25]
+    })
+    const titleTextBottom = this.loginHeight.interpolate({
+        inputRange: [150, 400, SCREEN_HEIGHT],
+        outputRange: [0, 0, 100]
+    })
+    const titleTextOpacity = this.loginHeight.interpolate({
+        inputRange: [150, SCREEN_HEIGHT],
+        outputRange: [0, 1]
+    })
+
+
+
     return (
-      <View style={{flex:1}}>
-        <ImageBackground
-        source= {require('../assets/login_bg.jpg')}
-        style={{flex: 1}}
-        >
-            <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-                <View style={{backgroundColor: 'white', height: 100, width: 200,  alignItems:'center', justifyContent:'center'}}>
-                    <Text style={{fontFamily: Fonts.Pacifico, fontWeight:'bold', fontSize:30}}>Dharamshala</Text>
-                    <Text style={{fontFamily: 'Pacifico', fontWeight:'bold', fontSize:30}}>Foods </Text>    
-                </View>
-            </View>
-            {/*footer */}
-            <View>
-                <View
-                    style={{
-                        height: 150,
-                        backgroundColor: 'white'
-                    }}>
+        <View style={{ flex: 1 }}>
 
-                    <View style={{
-                         opacity: 1,
-                         alignItems: 'flex-start',
-                         paddingHorizontal:25,
-                         marginTop: 25
-                    }}
-                       >
-                       <Text style={{ fontSize: 24}}>
-                       GET delivered Now
-                        </Text>   
+            <Animated.View
+                style={{
+                    position: 'absolute',
+                    height: 60, width: 60,
+                    top: 60,
+                    left: 25,
+                    zIndex: 100,
+                    opacity: headerBackArrowOpacity//animated
+                }}
+            >
+                <TouchableOpacity
+                    onPress={() => this.decreaseHeightOfLogin()}
 
-                    </View>
+
+
                     
-                    <TouchableOpacity>
-                        <View style={{
-                            marginTop: 25,
-                            paddingHorizontal:25,
-                             flexDirection: 'row'
-                        }}>
-                            <Image
-                                source={require('../assets/india.png')}
+                >
+                    <Icon name="md-arrow-back" style={{ color: 'black' }} />
+                </TouchableOpacity>
+            </Animated.View>
+
+            <Animated.View
+                style={{
+                    position: 'absolute',
+                    height: 60, width: 60,
+                    right: 10,
+                    bottom: this.keyboardHeight, // animated
+                    opacity: this.forwardArrowOpacity,//animated
+                    zIndex: 100,
+                    backgroundColor: '#54575e',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    borderRadius: 30
+                }}
+            >
+                <Icon name="md-arrow-forward" style={{ color: 'white' }} />
+            </Animated.View>
+
+            <ImageBackground
+                source={require('../assets/login_bg.jpg')}
+                style={{ flex: 1 }}
+            >
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                    <Animatable.View
+                        animation="zoomIn" iterationCount={1}
+                        style={{ backgroundColor: 'white', height: 100, width: 175, alignItems: 'center', justifyContent: 'center' }}>
+                        <Text style={{ fontWeight: 'bold',color:'black', fontSize: 26 }}>Dharamshala</Text>
+                        <Text style={{ fontWeight: 'bold',color:'black', fontSize: 26 }}>Foods</Text>
+                    </Animatable.View>
+                </View>
+
+                {/** BOTTOM HALF **/}
+                <Animatable.View animation="slideInUp" iterationCount={1}>
+
+                    <Animated.View
+                        style={{
+                            height: this.loginHeight,//animated
+                            backgroundColor: 'white'
+
+                        }}
+                    >
+                        <Animated.View
+                            style={{
+                                opacity: headerTextOpacity,//animated
+                                alignItems: 'flex-start',
+                                paddingHorizontal: 25,
+                                marginTop: marginTop//animated
+                            }}
+                        >
+                            <Text
+                                style={{ fontSize: 24, color:'black' }}
+                            >Get Food Delivered Now</Text>
+                        </Animated.View>
+
+                        <TouchableOpacity
+                            onPress={() => this.increaseHeightOfLogin()}
+                        >
+                            <Animated.View
                                 style={{
-                                    height:24,
-                                    width: 24,
-                                    paddingVertical: 23,
-                                    
-                                    resizeMode: 'contain'
-                                }}
-                            />
-                                <View style={{
+                                    marginTop: marginTop,//animated
+                                    paddingHorizontal: 25,
                                     flexDirection: 'row',
-                                    flex: 1,
-                                }}>
+                                }}
+                            >
+                                <Animated.Text
+                                    style={{
+                                        fontSize: 24, color: 'gray',
+                                        position: 'absolute',
+                                        bottom: titleTextBottom,//animated
+                                        left: titleTextLeft,//animated
+                                        opacity: titleTextOpacity//animated
+                                    }}
+                                >
+                                    Enter your mobile number
+                            </Animated.Text>
+
+
+                                <Image
+                                    source={require('../assets/india.png')}
+                                    style={{ height: 24, width: 24, resizeMode: 'contain', paddingVertical:23 }}
+                                />
+                                <Animated.View
+                                    pointerEvents="none"
+                                    style={{
+                                        flexDirection: 'row',
+                                        flex: 1,
+                                        borderBottomWidth: this.borderBottomWidth//animated
+                                    }}
+                                >
                                     <Text style={{
                                         fontSize: 20,
                                         paddingHorizontal: 10,
-                                        paddingVertical: 10
-                                    }}
-                                    >+91
-                                    </Text>
+                                        paddingVertical: 10,
+                                        color:'black'
+                                    }}>+91</Text>
 
-                                    <TextInput style={{
-                                        flex:1, fontSize:20}}
-                                        placeholder="enter your mobile number "
+                                    <TextInput
+                                        keyboardType="phone-pad"
+                                        ref="textInputMobile"
+                                        style={{ flex: 1, fontSize: 20 }}
+                                        placeholder={this.state.placeholderText}
                                         underlineColorAndroid="transparent"
-                                        
-                                        />
-                                        
+                                    />
+                                </Animated.View>
+                            </Animated.View>
+                        </TouchableOpacity>
 
-                                </View>
-                        </View>
-                    </TouchableOpacity>
+                    </Animated.View>
+                    <View
+                        style={{
+                            height: 70,
+                            backgroundColor: 'white',
+                            alignItems: 'flex-start',
+                            justifyContent: 'center',
+                            borderTopColor: '#e8e8ec',
+                            borderTopWidth: 1,
+                            paddingHorizontal: 25
+                        }}
+                    >
+                        <Text
+                            style={{
+                                color: '#5a7fdf', fontWeight: 'bold'
+                            }}
+                        >
+                            Or connect using a social account
+                        </Text>
+                    </View>
+                </Animatable.View>
+            </ImageBackground>
 
-                </View>
-                <View
-                    style={{
-                        height: 70,
-                        backgroundColor: 'white',
-                        justifyContent: 'center',
-                        alignItems:'flex-start',
-                        borderTopColor:'#e8e8ec',
-                        borderWidth: 1,
-                        paddingHorizontal: 25,
-                    }}>
-                    <Text style={{
-                        color:'#5a7fdf',
-                        fontWeight:'bold'
-                    }}>
-                        Or connect using a social network
-                    </Text>
-                
-                
-                </View>   
-                
-            </View>
-        </ImageBackground>
-      </View>
-    )
+        </View>
+    );
   }
 }
 
