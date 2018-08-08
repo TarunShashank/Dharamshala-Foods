c8b29a1e44e372f6ead16b29ae5fef4d9e6b0559
Tarun Shashank <stn.shashank@gmail.com>
2018-08-08 18:55:29 +0530

    checking functions using postman

--- dharamshalafoods-otplogin/functions/index.js
+++ dharamshalafoods-otplogin/functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+const createUser = require('./create-users');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -11,3 +12,5 @@ const functions = require('firebase-functions');
     response.send("GoodBye!");
    });
   
+
+exports.createUser = functions.https.onRequest(createUser);
